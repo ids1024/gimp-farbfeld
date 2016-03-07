95a368fe279c4580d1f91385ed35ccba13215bf1
Ian D. Scott <ian@iandouglasscott.com>
2016-03-06 17:22:42 -0800

    Load entire buffer at a time, not pixel by pixel
    Fixes slow speed

--- farbfeld.c
+++ farbfeld.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -50,7 +51,9 @@ static void run(const gchar *name, gint nparams, const GimpParam *param,
     FILE *file;
     uint8_t hdr[strlen("farbfeld") + 2 * sizeof(uint32_t)];
     uint16_t rgba[4];
-    guint8 pixel[4];
+    guchar *buf;
+    GimpDrawable *drawable;
+    GimpPixelRgn pixel_region;
 
     file = fopen(filename, "rb");
     fread(hdr, 1, sizeof(hdr), file);
@@ -62,15 +65,24 @@ static void run(const gchar *name, gint nparams, const GimpParam *param,
     layer = gimp_layer_new(image, "Background", width, height, GIMP_RGBA_IMAGE,
                            100.0, GIMP_NORMAL_MODE);
     gimp_image_insert_layer(image, layer, 0, 0);
+    drawable = gimp_drawable_get(layer);
+    gimp_pixel_rgn_init(&pixel_region, drawable, 0, 0, width,
+                        height, TRUE, FALSE);
+
+    buf = malloc(height * width * 4);
 
     for (i = 0; i < height; i++) {
       for (j = 0; j < width; j++) {
         fread(rgba, sizeof(uint16_t), 4, file);
         for (k = 0; k < 4; k++)
-          pixel[k] = ntohs(rgba[k]) / 257;
-        gimp_drawable_set_pixel(layer, j, i, 4, pixel);
+          buf[i*width*4 + j*4 + k] = ntohs(rgba[k]) / 257;
       }
     }
+
+    gimp_pixel_rgn_set_rect(&pixel_region, buf, 0, 0, width, height);
+    free(buf);
+    gimp_drawable_flush(drawable);
+    gimp_drawable_detach(drawable);
 
     *nreturn_vals = 2;
     values[0].type = GIMP_PDB_STATUS;
